21b2a81039b0a84310b1cfc763f031ec419b3151
Danny Marques <daninhacmmarques@gmail.com>
2024-10-07 12:10:38 -0300

    feature: foi criada a pagina layout de login e as validações, também foi criado o componente de botão e componente de input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AngularToastifyModule, ToastService } from 'angular-toastify';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './pages/login/login.module';
+import { PagesRoutingModule } from './pages/pages-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    LoginModule,
+    FontAwesomeModule,
+    AngularToastifyModule,
+    PagesRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ToastService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
