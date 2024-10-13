6e4a8929dac3a77c2215ae6a90c31ddd0bc5d3eb
Maria Suzane <suzaane97@gmail.com>
2024-10-13 16:34:13 -0300

    finalizando tela principal

--- src/utils/mocks/mocksCadrastro.ts
+++ src/utils/mocks/mocksCadrastro.ts
@@ -1,14 +1,17 @@
+
+
 export const mockCadastro=[{
     id:1,
-    titulo:"Cadastrar Turmas"
+    titulo:"Cadastrar Turmas",
+    imagem: "../../assets/image/turma1.png"
     },
     {
     id:2,
-    titulo:"Cadastrar Estudantes"
-    
+    titulo:"Cadastrar Estudantes",
+    imagem: "../../assets/image/estudante1.png"
     },{
     id:3,
-    titulo:"Cadastrar Professores"
-    
+    titulo:"Cadastrar Professores",
+    imagem: "../../assets/image/prof1.png"
     }
     ]
