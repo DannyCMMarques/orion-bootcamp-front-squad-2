1c0840059c6fc30331ec68adf86965bd2a2fdbf1
Maria Suzane <suzaane97@gmail.com>
2024-10-17 18:51:05 -0300

    adicionando responsividade na tela principal e cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TelaPrincipalComponent } from './pages/tela-principal/tela-principal.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [{
-  path: 'tela-principal', component: TelaPrincipalComponent
 }]
 
 @NgModule({
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { PageExibirCadastrosComponent } from './page-exibir-cadastros/page-exibir-cadastros.component';
 import { PagesCadatrarComponent } from './pages-cadatrar/pages-cadatrar.component';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tela-inicial', pathMatch: 'full' },
   { path: 'tela-inicial', component: TelaInicialComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'tela-principal', component: TelaPrincipalComponent},
   {
     path: 'cadastrados/:type',
     component: PageExibirCadastrosComponent,
