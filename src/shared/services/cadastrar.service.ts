09ee89310db5f8fb83e8d1acc3eb9c9812631a83
Danny Marques <daninhacmmarques@gmail.com>
2024-10-23 10:39:23 -0300

    feat : cria o service cadastrar

--- src/shared/services/cadastrar.service.ts
+++ src/shared/services/cadastrar.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class CadastrarService {
+  public api_path = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.api_path = environment.urlBase;
+  }
+
+  public cadastroEstudante(payload: any): Observable<any> {
+    //  TODO:Mudar quando tiver o endPoint
+    return this.http
+      .post(`${this.api_path}cadastrar/Estudante`, payload);
+  }
+
 }
