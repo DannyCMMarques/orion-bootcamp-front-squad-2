c30070d328b03cc21e074faf77329dfeae3a1fef
Danny Marques <daninhacmmarques@gmail.com>
2024-10-23 09:51:19 -0300

    feat : implementa o método onSubmit, o toast e o dialog.

--- src/shared/components/shared-component.module.ts
+++ src/shared/components/shared-component.module.ts
@@ -2,18 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AngularToastifyModule, ToastService } from 'angular-toastify';
 import { ButtonTelaInicialComponent } from './button-tela-inicial/button-tela-inicial.component';
 import { ButtonComponent } from './button/button.component';
 import { ContentTelaComponent } from './content-tela/content-tela.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { FormularioEstudanteComponent } from './formulario-estudante/formulario-estudante.component';
+import { FormularioTurmasComponent } from './formulario-turmas/formulario-turmas.component';
 import { ImagemTelaComponent } from './imagem-tela/imagem-tela.component';
 import { InputFieldComponent } from './input-component/input-component.component';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { OpcoesMenuLateralComponent } from './opcoes-menu-lateral/opcoes-menu-lateral.component';
 import { SelectComponent } from './select/select.component';
-import { FormularioTurmasComponent } from './formulario-turmas/formulario-turmas.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     DialogComponent,
     FormularioTurmasComponent,
   ],
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, AngularToastifyModule,
+  ],
   exports: [
     ButtonComponent,
     InputFieldComponent,
@@ -42,5 +43,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     DialogComponent,
     FormularioTurmasComponent,
   ],
+  providers:[
+    ToastService
+  ]
 })
 export class SharedComponentModule {}
